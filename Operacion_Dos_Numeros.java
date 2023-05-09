488613c5a2dbf550fbbe2ade32aa4ab20eebcbde
jeisonruizruiz <yeisonruizruiz069@gmail.com>
2023-05-08 22:07:51 -0500

    añadir tercer punto taller

---
+++ Operacion_Dos_Numeros.java
@@ -0,0 +1,34 @@
+
+import javax.swing.JOptionPane;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author jeiso
+ */
+public class Operacion_Dos_Numeros {
+    
+    
+  public static void main(String[] args) {
+        int numeroA = 89;
+        int numeroB = 323;
+        
+        int suma = numeroA + numeroB;
+        int resta = numeroA - numeroB;
+        int division = numeroB / numeroA;
+        int multiplicacion = numeroA * numeroB;
+        
+        System.out.println("Valor de numeroA: " + numeroA);
+        System.out.println("Valor de numeroB: " + numeroB);
+        System.out.println("Suma: " + suma);
+        System.out.println("Resta: " + resta);
+        System.out.println("Division: " + division);
+        System.out.println("Multiplicacion: " + multiplicacion);
+    }
+}
+    
+
