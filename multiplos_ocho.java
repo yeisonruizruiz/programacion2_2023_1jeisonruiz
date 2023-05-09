a81a1b0b99a62e5654e132345add2bf8a6f761f9
jeisonruizruiz <yeisonruizruiz069@gmail.com>
2023-05-08 22:32:54 -0500

    añadir el cuarto punto taller

---
+++ multiplos_ocho.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author jeiso
+ */
+public class multiplos_ocho {
+    public static void main(String[] args) {
+        int i = 0;
+while (i <= 100) {
+  if (i % 8 == 0) {
+    System.out.println(i);
+  }
+  i++;
+}
+    }
+    
+}
